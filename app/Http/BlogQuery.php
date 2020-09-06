c0db6aa12a591ad264f42f489aee471c871ab3b2
Pranay Patro <pranaypatro11@gmail.com>
2020-09-06 21:09:46 +0530

    Chore: returned Tags while fetching  blog details

--- app/Http/BlogQuery.php
+++ app/Http/BlogQuery.php
@@ -5,11 +5,24 @@ namespace App\Http;
 
 
 use App\Blog;
+use App\BlogTagMapping;
+use App\Tag;
 
 class BlogQuery {
 
     public static function getBlog($slug) {
-        return Blog::select('title_slug', 'title', 'data', 'creator')->where('title_slug', '=', $slug)->get()->toArray()[0];
+        $blog = Blog::select('id', 'title_slug', 'title', 'data', 'creator')
+                ->where('title_slug', '=', $slug)
+                ->get()
+                ->toArray()[0];
+        $tagData = BlogTagMapping::select('tag_name')
+                ->where('blog_tag_mapping.blog_id', '=', $blog['id'])
+                ->join('tags', 'blog_tag_mapping.tag_id', '=', 'tags.id')
+                ->get()->pluck('tag_name')->toArray();
+        $blog['tags'] = $tagData;
+
+
+        return $blog;
     }
 
 }
